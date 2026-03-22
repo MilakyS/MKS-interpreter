41b4319191ac7f5a627b3a7beea69ff52c357710
MilakyS <rm3961390@gmail.com>
2026-03-22 19:43:07 +0300

    Windows update

--- Parser/parser.c
+++ Parser/parser.c
@@ -4,7 +4,19 @@
 #include <string.h>
 #include "../Utils/hash.h"
 
-// --- Хелперы ---
+
+static char *mks_strndup(const char *s, size_t n) {
+    size_t len = 0;
+    while (len < n && s[len]) {
+        len++;
+    }
+    char *copy = (char *)malloc(len + 1);
+    if (copy) {
+        memcpy(copy, s, len);
+        copy[len] = '\0';
+    }
+    return copy;
+}
 
 void parser_advance(Parser *parser) {
     if (parser->lexer != NULL) parser->current_token = lexer_next(parser->lexer);
@@ -27,7 +39,6 @@ enum TokenType peek_token_type(Parser *parser) {
     return next_token.type;
 }
 
-// Прототипы для рекурсивного спуска
 ASTNode* parser_parse_expression(Parser *parser);
 ASTNode* parser_parse_logical_and(Parser *parser);
 ASTNode* parser_parse_comparison(Parser *parser);
@@ -37,8 +48,6 @@ ASTNode* parser_parse_term(Parser *parser);
 ASTNode* parser_parse_factor(Parser *parser);
 ASTNode* parser_parse_statement(Parser *parser);
 ASTNode* parser_parse_block(Parser *parser);
-
-// --- Парсинг Выражений (Expression) ---
 
 ASTNode* parser_parse_expression(Parser *parser) {
     ASTNode *node = parser_parse_logical_and(parser);
@@ -50,24 +59,17 @@ ASTNode* parser_parse_expression(Parser *parser) {
         node = create_ast_binop(node, parser_parse_logical_and(parser), op, line);
     }
 
-    // Присваивание =:
     if (parser->current_token.type == TOKEN_ASSIGN) {
         int line = parser->current_token.line;
         parser_eat(parser, TOKEN_ASSIGN);
         ASTNode *rhs = parser_parse_expression(parser);
 
-        // x =: 5 -> Быстрое присваивание по хешу
         if (node->type == AST_IDENTIFIER) {
-            // 🔥 ФИКС: Создаем узел присваивания...
             ASTNode *assign_node = create_ast_assign(node->data.identifier.name, node->data.identifier.id_hash, rhs, line);
-
-            // 🔥 УБИВАЕМ ПУСТУЮ КОРОБКУ ОТ ИДЕНТИФИКАТОРА (ТЕ САМЫЕ 48 БАЙТ!)
             free(node);
 
             return assign_node;
         }
-
-        // arr[0] =: 5 -> Присваивание по индексу
         return create_ast_index_assign(node, rhs, line);
     }
 
@@ -133,21 +135,18 @@ ASTNode* parser_parse_factor(Parser *parser) {
     ASTNode *node = NULL;
     int line = parser->current_token.line;
 
-    // 1. Числа
     if (parser->current_token.type == TOKEN_TYPE_NUMBER) {
         double val = parser->current_token.double_value;
         parser_eat(parser, TOKEN_TYPE_NUMBER);
         node = create_ast_num(val, line);
     }
-    // 2. Строки
     else if (parser->current_token.type == TOKEN_TYPE_STRING) {
-        char *str = strndup(parser->current_token.start, parser->current_token.length);
+        char *str = mks_strndup(parser->current_token.start, parser->current_token.length);
         parser_eat(parser, TOKEN_TYPE_STRING);
         node = create_ast_string(str, line);
     }
-    // 3. Идентификаторы и вызовы функций
     else if (parser->current_token.type == TOKEN_IDENTIFIER) {
-        char *name = strndup(parser->current_token.start, parser->current_token.length);
+        char *name = mks_strndup(parser->current_token.start, parser->current_token.length);
         unsigned int name_hash = get_hash(name);
         parser_eat(parser, TOKEN_IDENTIFIER);
 
@@ -167,7 +166,6 @@ ASTNode* parser_parse_factor(Parser *parser) {
             node = create_ast_ident(name, name_hash, line);
         }
     }
-    // 4. Массивы []
     else if (parser->current_token.type == TOKEN_LBRACKET) {
         parser_eat(parser, TOKEN_LBRACKET);
         size_t cap = 10, count = 0;
@@ -182,7 +180,6 @@ ASTNode* parser_parse_factor(Parser *parser) {
         parser_eat(parser, TOKEN_RBRACKET);
         node = create_ast_array(elements, (int)count, line);
     }
-    // 5. Группировка ()
     else if (parser->current_token.type == TOKEN_LPAREL) {
         parser_eat(parser, TOKEN_LPAREL);
         node = parser_parse_expression(parser);
@@ -193,7 +190,6 @@ ASTNode* parser_parse_factor(Parser *parser) {
         exit(1);
     }
 
-    // ПОСТФИКСЫ: Индексы a[i] и Методы a.len()
     while (parser->current_token.type == TOKEN_LBRACKET || parser->current_token.type == TOKEN_DOT) {
         if (parser->current_token.type == TOKEN_LBRACKET) {
             parser_eat(parser, TOKEN_LBRACKET);
@@ -203,13 +199,12 @@ ASTNode* parser_parse_factor(Parser *parser) {
         }
         else if (parser->current_token.type == TOKEN_DOT) {
             parser_eat(parser, TOKEN_DOT);
-            char *m_name = strndup(parser->current_token.start, parser->current_token.length);
+            char *m_name = mks_strndup(parser->current_token.start, parser->current_token.length);
             unsigned int m_hash = get_hash(m_name);
             parser_eat(parser, TOKEN_IDENTIFIER);
 
             ASTNode **args = calloc(16, sizeof(ASTNode*));
             int count = 0;
-            // Методы могут вызываться как свойства (без скобок) или как функции
             if (parser->current_token.type == TOKEN_LPAREL) {
                 parser_eat(parser, TOKEN_LPAREL);
                 if (parser->current_token.type != TOKEN_RPAREL) {
@@ -226,23 +221,20 @@ ASTNode* parser_parse_factor(Parser *parser) {
     return node;
 }
 
-// --- Инструкции (Statements) ---
 
 ASTNode* parser_parse_statement(Parser *parser) {
     enum TokenType type = parser->current_token.type;
     int line = parser->current_token.line;
 
-    // using "lib" as alias;
     if (type == TOKEN_KW_USING) {
         parser_eat(parser, TOKEN_KW_USING);
-        char *path = strndup(parser->current_token.start, parser->current_token.length);
+        char *path = mks_strndup(parser->current_token.start, parser->current_token.length);
         parser_eat(parser, TOKEN_TYPE_STRING);
         char *alias = NULL;
         if (parser->current_token.type == TOKEN_IDENTIFIER) {
-            // Умная проверка на 'as'
             if (parser->current_token.length == 2 && strncmp(parser->current_token.start, "as", 2) == 0) {
-                parser_advance(parser); // Пропускаем 'as'
-                alias = strndup(parser->current_token.start, parser->current_token.length);
+                parser_advance(parser);
+                alias = mks_strndup(parser->current_token.start, parser->current_token.length);
                 parser_eat(parser, TOKEN_IDENTIFIER);
             }
         }
@@ -250,10 +242,9 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_using(path, alias, line);
     }
 
-    // var x =: 5;
     if (type == TOKEN_KW_VAR) {
         parser_eat(parser, TOKEN_KW_VAR);
-        char *name = strndup(parser->current_token.start, parser->current_token.length);
+        char *name = mks_strndup(parser->current_token.start, parser->current_token.length);
         unsigned int v_hash = get_hash(name);
         parser_eat(parser, TOKEN_IDENTIFIER);
         parser_eat(parser, TOKEN_ASSIGN);
@@ -262,7 +253,6 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_var_decl(val, line, name, v_hash);
     }
 
-    // if (cond) { ... } else { ... }
     if (type == TOKEN_KW_IF) {
         parser_eat(parser, TOKEN_KW_IF);
         parser_eat(parser, TOKEN_LPAREL);
@@ -272,13 +262,11 @@ ASTNode* parser_parse_statement(Parser *parser) {
         ASTNode *else_b = NULL;
         if (parser->current_token.type == TOKEN_KW_ELSE) {
             parser_eat(parser, TOKEN_KW_ELSE);
-            // Поддержка else if
             else_b = (parser->current_token.type == TOKEN_KW_IF) ? parser_parse_statement(parser) : parser_parse_block(parser);
         }
         return create_if_block(cond, body, else_b, line);
     }
 
-    // while (cond) { ... }
     if (type == TOKEN_KW_WHILE) {
         parser_eat(parser, TOKEN_KW_WHILE);
         parser_eat(parser, TOKEN_LPAREL);
@@ -287,7 +275,6 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_while_block(cond, parser_parse_block(parser), line);
     }
 
-    // for (init; cond; step) { ... }
     if (type == TOKEN_KW_FOR) {
         parser_eat(parser, TOKEN_KW_FOR);
         parser_eat(parser, TOKEN_LPAREL);
@@ -297,7 +284,7 @@ ASTNode* parser_parse_statement(Parser *parser) {
             if (parser->current_token.type == TOKEN_KW_VAR) {
                 int v_line = parser->current_token.line;
                 parser_eat(parser, TOKEN_KW_VAR);
-                char *v_name = strndup(parser->current_token.start, parser->current_token.length);
+                char *v_name = mks_strndup(parser->current_token.start, parser->current_token.length);
                 unsigned int v_hash = get_hash(v_name);
                 parser_eat(parser, TOKEN_IDENTIFIER);
                 parser_eat(parser, TOKEN_ASSIGN);
@@ -318,7 +305,6 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_for(init, cond, step, parser_parse_block(parser));
     }
 
-    // Writeln(...) / Write(...)
     if (type == TOKEN_KW_WRITELN || type == TOKEN_KW_WRITE) {
         bool nl = (type == TOKEN_KW_WRITELN);
         parser_advance(parser);
@@ -336,10 +322,9 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_output(args, count, nl, line);
     }
 
-    // fnc name(params) { ... }
     if (type == TOKEN_KW_FNC) {
         parser_eat(parser, TOKEN_KW_FNC);
-        char *f_name = strndup(parser->current_token.start, parser->current_token.length);
+        char *f_name = mks_strndup(parser->current_token.start, parser->current_token.length);
         parser_eat(parser, TOKEN_IDENTIFIER);
         parser_eat(parser, TOKEN_LPAREL);
         char **params = calloc(10, sizeof(char*));
@@ -347,7 +332,7 @@ ASTNode* parser_parse_statement(Parser *parser) {
         if (parser->current_token.type != TOKEN_RPAREL) {
             do {
                 if (p_count > 0) parser_eat(parser, TOKEN_COMMA);
-                params[p_count++] = strndup(parser->current_token.start, parser->current_token.length);
+                params[p_count++] = mks_strndup(parser->current_token.start, parser->current_token.length);
                 parser_eat(parser, TOKEN_IDENTIFIER);
             } while (parser->current_token.type == TOKEN_COMMA);
         }
@@ -355,7 +340,6 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_func_decl(f_name, params, p_count, parser_parse_block(parser), line);
     }
 
-    // return expr;
     if (type == TOKEN_KW_RETURN) {
         parser_eat(parser, TOKEN_KW_RETURN);
         ASTNode *val = parser_parse_expression(parser);
@@ -363,7 +347,6 @@ ASTNode* parser_parse_statement(Parser *parser) {
         return create_ast_return(val, line);
     }
 
-    // Обычное выражение как инструкция
     ASTNode *expr = parser_parse_expression(parser);
     if (parser->current_token.type == TOKEN_SEMICOLON) parser_eat(parser, TOKEN_SEMICOLON);
     return expr;
